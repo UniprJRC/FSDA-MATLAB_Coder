e85f80b1c82322a88b762ce6936b103b78fcf0f8
Aldo Corbellini <aldo.corbellini@unipr.it>
2021-02-03 19:06:54 +0100

    edited the testing script

--- addt/TestAddtMex.m
+++ addt/TestAddtMex.m
@@ -1,6 +1,4 @@
 %% addt with optional arguments.
-% Excluding one observation from the sample; compare the added variable plot
-% based on all units with that which excludes unit 43.
 
 rng('default')
 rng(100)
@@ -9,7 +7,6 @@ n=50;
 y=randn(n,1)+10;
 X=randn(n,3);
 w=randn(n,1);
-
 
 
 %la=[];
@@ -28,7 +25,6 @@ defCell ={'1'};
 
 [out1]=addt(y, X, w, 'plots', 0, 'intercept', 1, 'la', la, 'nocheck', 0, ...
     'FontSize', 12, 'SizeAxesNum', 10);
-
 
 disp(out)
 disp(out1)
