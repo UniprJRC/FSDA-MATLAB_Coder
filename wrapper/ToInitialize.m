00baae1e033d6fb2100e5ad0831b0c1af8ea757b
Marco Riani <mriani@unipr.it>
2021-06-27 23:55:51 +0200

    Bug corrected in ToInitialize about fwrite

--- wrapper/ToInitialize.m
+++ wrapper/ToInitialize.m
@@ -13,7 +13,7 @@ newpwd=pwd;
 % Open Excel file
 X=readtable([OldFolder fsep 'ToInitialize.xlsx']);
 n=size(X,1);
-for i=1:n
+for i=2:n
     disp(['Row ' num2str(i)])
     FileName=X{i,1}; % cell format
     FileName=FileName{1,1}; % character format
@@ -27,27 +27,27 @@ for i=1:n
         % find and replace in all C files contained in subfolder
         % codegen/lib/fsdaC
         d = dir([newpwd filesep '*.c']);
-        %   if max(strcmp(fieldnames(d),'folder'))>0
-        %             d=rmfield(d,'folder');
-        %   end
         
         for ii = 1:length(d)
             FileName=d(ii).name;
             FileWithFullPath=which(FileName);
             [pathstrcf,name,ext]=fileparts(FileWithFullPath);
-            fileID = fopen(FileWithFullPath, 'w');
+            fileID = fopen(FileWithFullPath, 'r');
             if fileID==-1
                 error('FSDA:ToInitialize:WrongFile',['Could not find file ' '''' FileName ''''])
             end
             
             % Insert the file into fstring
             fstring=fscanf(fileID,'%c');
+                    fclose(fileID);
             
             % Perform search and replace
             fstringNEW=strrep(fstring,strTofind,strToReplace);
-            % Write into fileID
-            nb=fprintf(fileID,'%s',fstringNEW);
-            status=fclose(fileID);
+            fileID1 = fopen(FileWithFullPath, 'w');
+
+            % Write into fileID1
+            nb=fprintf(fileID1,'%s',fstringNEW);
+            status=fclose(fileID1);
             if status ~=0
                 error('FSDA:ToInitialize:FileNotClosed',['Could not close the connection with file: ' '''' FileName ''''])
             end
@@ -57,13 +57,14 @@ for i=1:n
         % find and replace just inside FileName
         FileWithFullPath=which(FileName);
         [pathstrcf,name,ext]=fileparts(FileWithFullPath);
-        fileID = fopen(FileWithFullPath, 'w');
+        fileID = fopen(FileWithFullPath, 'r');
         if fileID==-1
             error('FSDA:ToInitialize:WrongFile',['Could not find file ' '''' FileName ''''])
         end
         
         % Insert the file into fstring
         fstring=fscanf(fileID,'%c');
+        fclose(fileID);
         
         sIndexTofind = regexp(fstring,strTofind);
         sIndexToreplace = regexp(fstring,strToReplace);
@@ -73,16 +74,23 @@ for i=1:n
         elseif length(sIndexTofind)>1
             warning('FSDA:ToInitialize:WrongString',['String ' '''' strTofind '''' ' found more than once in file ' '''' FileName ''''])
         elseif ~isempty(sIndexToreplace)
-            error('FSDA:ToInitialize:WrongString',['String ' '''' sIndexToreplace '''' ' already exists in input file ' '''' FileName ''''])
+            error('FSDA:ToInitialize:WrongString',['String ' '''' strToReplace '''' ' already exists in input file ' '''' FileName ''''])
         else
         end
         % Perform search and replace
         fstring=regexprep(fstring,strTofind,strToReplace);
         % Write into fileID
-        fprintf(fileID,'%s',fstring);
-        status=fclose(fileID);
+        fileID1 = fopen(FileWithFullPath, 'w');
+        if fileID1==-1
+            error('FSDA:ToInitialize:WrongFile',['Could not find file ' '''' FileName ''''])
+        end
+      
+        fprintf(fileID1,'%s',fstring);
+        status=fclose(fileID1);
         if status ~=0
             error('FSDA:ToInitialize:FileNotClosed',['Could not close the connection with file: ' '''' FileName ''''])
         end
     end
 end
+
+cd(OldFolder)
