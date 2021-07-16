8fdfc708179f562b9bcd200388598fc480b12fc7
valentint <valentin.todorov@chello.at>
2021-07-17 00:49:06 +0200

    Update Critical-bugs.R

--- R-package/inst/work/Critical-bugs.R
+++ R-package/inst/work/Critical-bugs.R
@@ -1,7 +1,9 @@
 ##  1. Crash of FSRmdr() with a specific initial data set (bsb)
 ##      with a message of the following type
 ##      'FSDA:FSRmdr, Rank problem in step 10'
-
+##
+##  Fixed now, manually changing the file FSRmdr_wrapper
+##
     library(fsdac)
     data(hawkins)
     y <- hawkins[, 9, drop=FALSE]
@@ -15,3 +17,19 @@
     out1 <- FSRmdr(y, X, bsb=bsb, bsbsteps=c(30, 60))
 
     as.vector(out1$mdr)
+
+##  2. Crash of FSR, which was not there before compiling with linsolve()
+##      The problem actually occurs in the call to FSRmdr()
+##
+##  Example of point mass contamination.
+    library(fsdac)
+    n <- 130
+    p <- 5
+    set.seed(12345678)
+    X <- matrix(rnorm(n*p), nrow=n)
+    y <- rnorm(n)
+    kk <- 30
+    ##  point mass contamination of the first kk units
+    X[1:kk,] <- 1
+    y[1:kk] <- 3
+    (out <- FSR(y, X))
